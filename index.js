0bf5862c6168f03587ca463f964849ac2ac758f1
sachincoolkarni <sachincoolkarni@users.noreply.github.com>
2016-08-04 12:34:30 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var mod=angular.module("myapp",[]);
+mod.controller("mycontroller",["$scope",function($scope)
+
+{
+	$scope.msg="ng-bind is angular built in directive and it is widely used in the programming"
+
+
+
+}]);
